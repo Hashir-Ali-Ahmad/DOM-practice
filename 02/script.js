5199e72635786f42976dad730e8e130a1a8053e2
Hashir Ali Ahmad <hashir.47a@gmail.com>
2024-08-14 15:32:40 +0500

    JS edited

--- 02/script.js
+++ 02/script.js
@@ -2,10 +2,10 @@ var img1 = document.querySelector('.img1');
 var img2 = document.querySelector('.img2');
 var btn = document.querySelector('button');
 
-var src1 = img1.src;
-var src2 = img1.sr2;
 
 btn.addEventListener('click',()=>{
+    var src1 = img1.src;
+    var src2 = img2.src;
     img1.src = src2;
     img2.src = src1;
 });
